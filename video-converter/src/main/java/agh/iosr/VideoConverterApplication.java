ab4467747cbc3de38014aab9031764f6e80fa6c4
Piotr Martyna <piotrek722@gmail.com>
2017-12-08 17:43:43 +0100

    project initial structure

---
+++ video-converter/src/main/java/agh/iosr/VideoConverterApplication.java
@@ -0,0 +1,12 @@
+package agh.iosr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoConverterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideoConverterApplication.class, args);
+	}
+}
