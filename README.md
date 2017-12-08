1cf53b8a81cdf6640dd9d595521a8638c9d1e34b
Piotr Martyna <piotrek722@gmail.com>
2017-12-08 22:34:00 +0100

    added docker

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # VideoConverter
+
+### Build docker image
+./gradlew clean build buildDocker
+
+### Run application
+docker run -p 8080:8080 -t video-converter
--- video-converter/src/main/java/agh/iosr/VideoConverterApplication.java
+++ video-converter/src/main/java/agh/iosr/VideoConverterApplication.java
@@ -2,9 +2,18 @@ package agh.iosr;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class VideoConverterApplication {
+
+	@GetMapping
+	public ResponseEntity<String> test(){
+		return ResponseEntity.ok("test");
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(VideoConverterApplication.class, args);
